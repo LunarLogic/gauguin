3633bd449a518dd7a9d04b388ba2119f3931b07e
Ania Slimak <anna.slimak@lunarlogic.io>
2014-11-25 10:53:13 +0100

    Enable configuring similarity treshold

--- lib/gauguin.rb
+++ lib/gauguin.rb
@@ -16,13 +16,16 @@ module Gauguin
     DEFAULT_MAX_COLORS_COUNT = 10
     DEFAULT_CUT_OFF_LIMIT = 1000
     DEFAULT_MIN_DIFF = 0.003
+    DEFAULT_COLOR_SIMILARITY_THRESHOLD = 50
 
-    attr_accessor :max_colors_count, :cut_off_limit, :min_diff
+    attr_accessor :max_colors_count, :cut_off_limit,
+      :min_diff, :color_similarity_threshold
 
     def initialize
       @max_colors_count = DEFAULT_MAX_COLORS_COUNT
       @cut_off_limit = DEFAULT_CUT_OFF_LIMIT
       @min_diff = DEFAULT_MIN_DIFF
+      @color_similarity_threshold = DEFAULT_COLOR_SIMILARITY_THRESHOLD
     end
   end
 end
--- lib/gauguin/color.rb
+++ lib/gauguin/color.rb
@@ -2,20 +2,19 @@ require "matrix"
 
 module Gauguin
   class Color < Struct.new(:red, :green, :blue, :percentage)
-    SIMILARITY_THRESHOLD = 50
     YUV_MATRIX = Matrix[[0.299, 0.587, 0.114],
                         [-0.14713, -0.28886, 0.436],
                         [0.615, -0.51499, -0.10001]]
 
     def ==(other_color)
-      self.distance(other_color) < SIMILARITY_THRESHOLD
+      self.distance(other_color) < Gauguin.configuration.color_similarity_threshold
     end
 
     def distance(other_color)
       vector = self.to_yuv.to_a.flatten
       other_vector = other_color.to_yuv.to_a.flatten
-      squares = vector.map.with_index do |o, i|
-        (other_vector[i]-o)**2
+      squares = vector.map.with_index do |_, i|
+        (other_vector[i] - vector[i]) ** 2
       end
       sum = 0
       squares.each { |o| sum += o }
